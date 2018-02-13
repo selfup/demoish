974025d0bcdcd184d29da6ad7a3fda288a82025b
selfup <boudinot.regis@yahoo.com>
2018-02-12 19:09:21 -0600

    Add run script - fix lint error LocationId.java

--- src/main/java/com/selfup/demo/tables/LocationIds.java
+++ src/main/java/com/selfup/demo/tables/LocationIds.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Map;
 
 public class LocationIds {
-    public Map<String, List> records;
+    public Map<String, List<String>> records;
 
     public LocationIds() {
         this.records = new HashMap<>();
